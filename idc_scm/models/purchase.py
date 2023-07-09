865dee411888f849f68777aca8bbc6ef9934735c
farihatabassumeshita <fariha.eshita@gmail.com>
2023-07-09 18:51:04 +0600

    Shipment fields, view, menu name change

--- idc_scm/models/purchase.py
+++ idc_scm/models/purchase.py
@@ -5,7 +5,7 @@ from odoo import api, fields, models
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
-    state = fields.Selection(selection_add=[('draft','Purchase Order'),('sent','Purchase Order Sent'),('purchase','Proforma Invoice'),('lc_confirm', 'Letter of Credit Confirm'),('done', 'Done')],tracking=True,ondelete={'lc_confirm':'cascade'})
+    state = fields.Selection(selection_add=[('draft','Purchase Order'),('sent','Purchase Order Sent'),('purchase','Proforma Invoice'),('lc_confirm', 'Letter of Credit Confirm'),('done', '')],tracking=True,ondelete={'lc_confirm':'cascade'})
     order_date = fields.Date('Order Date', default=fields.Date.today())
     approx_arrive_date = fields.Date('Approx. Shipment Date', default=fields.Date.today())
     pi_date = fields.Date('Proforma Invoice Date', default=fields.Date.today())
@@ -20,8 +20,6 @@ class PurchaseOrder(models.Model):
     incoterm = fields.Many2one(
         'account.incoterms', 'Incoterm',
         help="International Commercial Terms are a series of predefined commercial terms used in international transactions.")
-
-
 
     def action_in_lc(self):
         for rec in self:
